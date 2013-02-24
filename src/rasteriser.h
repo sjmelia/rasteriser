0b388d5a13d1840ade61b659fb208f2f4fd49bf7
Steve Melia <steve.j.melia@gmail.com>
2013-02-24 07:11:58 +0000

    Clean up rasteriser header

--- src/rasteriser.h
+++ src/rasteriser.h
@@ -2,6 +2,7 @@
 #define RASTERISER_H
 
 #include "matrix4.h"
+#include "vector4.h"
 
 typedef struct rasteriser
 {
@@ -13,5 +14,5 @@ rasteriser* rasteriser_create();
 void rasteriser_free(rasteriser* rast);
 void rasteriser_translate(rasteriser* rast, double x, double y, double z, double w);
 void rasteriser_rotate(rasteriser* rast, double angle, double x, double y, double z, double w);
-
+void rasteriser_transform(rasteriser* rast, vector4* result, vector4* point);
 #endif
